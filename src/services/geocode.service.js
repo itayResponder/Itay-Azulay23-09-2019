0bdf9bd036224df5a2e9f1442374e3cbe683cba9
Itay Azulay <itay.responder@gmail.com>
2019-09-30 09:35:02 +0300

    change to https

--- src/services/geocode.service.js
+++ src/services/geocode.service.js
@@ -9,17 +9,17 @@ export default {
 }
 
 function getAutoComplete(search) {
-    return Axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${search}`)
+    return Axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${search}`)
 }
 
 function getWeatherCity(cityCode) {
-    return Axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${API_KEY}`)
+    return Axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${API_KEY}`)
 }
 
 function getWeatherForecast(cityCode, isMetric=true) {
-    return Axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityCode}?apikey=${API_KEY}&metric=${isMetric}`)
+    return Axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityCode}?apikey=${API_KEY}&metric=${isMetric}`)
 }
 
 function getCurrLocation(lat, lon) {
-    return Axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${lat}%2C${lon}`)
+    return Axios.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${lat}%2C${lon}`)
 }
