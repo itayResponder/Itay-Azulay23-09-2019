472039fe314fc5635055f65c1aeeb9e1604a47ae
Itay Azulay <itay.responder@gmail.com>
2019-09-30 10:08:07 +0300

    deploy

---
+++ precache-manifest.12272964e65ccffa4adc72efade2fe62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Itay-Azulay23-09-2019/robots.txt"
+  },
+  {
+    "revision": "d65ce44803d7ce9e57c4",
+    "url": "/Itay-Azulay23-09-2019/js/chunk-vendors.dbc11127.js"
+  },
+  {
+    "revision": "d0fa820f28f47f016c52",
+    "url": "/Itay-Azulay23-09-2019/js/app.8a247eb8.js"
+  },
+  {
+    "revision": "ff50505c0b45161e68c73e6c84faf20b",
+    "url": "/Itay-Azulay23-09-2019/index.html"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/Itay-Azulay23-09-2019/img/heart-multiple.e87899ae.svg"
+  },
+  {
+    "revision": "ec6223055097995a57e4bc7153d946dd",
+    "url": "/Itay-Azulay23-09-2019/img/heart-multiple-outline.ec622305.svg"
+  },
+  {
+    "revision": "d65ce44803d7ce9e57c4",
+    "url": "/Itay-Azulay23-09-2019/css/chunk-vendors.c9ac70e6.css"
+  },
+  {
+    "revision": "d0fa820f28f47f016c52",
+    "url": "/Itay-Azulay23-09-2019/css/app.c7f835fb.css"
+  }
+];
